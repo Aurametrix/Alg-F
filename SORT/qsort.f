8aac5d5e2d14c4dbd8940f4cc07410e0b5d8a781
Aurametrix <irene@aurametrix.com>
2015-08-19 15:03:45 -0700

    quicksort

--- SORT/qsort.f
+++ SORT/qsort.f
@@ -1,14 +1,24 @@
-PROGRAM xsort
-C driver for routine sort
+    PROGRAM xsort
+C   driver for routine sort
+    INTEGER i,j
+    REAL a(100)
+    open (7,file="TARRAY.DAT'. status='OLD')
+    read(7,*) (a(i), i=1,100)
+    close(7)
+
 C print original array
     write(*,*) 'Original array'
-C sort array
-C print sorted array
-    write(*,*) 'Sorted array'
-    write(*,*) '...and array B is:'
-    do 13 i=1,10
-      write(*,'(1x,10f7.2)') (b(10*(i-1)+j), j=1,10
-13  continue
-      write(*,*) 'press RETURN to continue...'
-read(*,*)
-C sort B and mix A
+
+    do 11 i=1,10
+      write(*,'(1x,10f7.2)') (a(10*(i-1)+j), j=1,10)
+11  continue
+
+C   sort array
+
+    call sort(100,a)
+C   print sorted array
+    write(*,*) 'Sorted array:'
+    do 12 i-1,10
+        write(*,'(1x,10f7.2)') (a(10*(i-1)+j), j=1,10)
+12  continue
+END
