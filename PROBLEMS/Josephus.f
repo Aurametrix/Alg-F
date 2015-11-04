05d1f0d67f796d015d66fbdc733b43c3cc9ed617
Aurametrix <irene@aurametrix.com>
2015-11-03 20:06:35 -0800

    interactive shell

--- PROBLEMS/Josephus.f
+++ PROBLEMS/Josephus.f
@@ -20,3 +20,27 @@ program josephus
    print *, "Alive", p
    deallocate(next)
 end program
+
+! friendly interactive shell
+!
+function execute
+    # If the list is empty, don't do anything.
+    test (count $argv) -ge 2; or return
+    # If the list has only one element, return it
+    if test (count $argv) -eq 2
+        echo $argv[2]
+        return
+    end
+    # Rotate prisoners
+    for i in (seq 2 $argv[1])
+        set argv $argv[1 3..-1 2]
+    end
+    # Mention killed prisoner
+    echo $argv[2]
+    # Kill rest recursively
+    execute $argv[1 3..-1]
+end
+ 
+echo Prisoner (execute 3 (seq 0 40))[-1] survived.
+echo Prisoners (execute 3 (seq 0 40))[-3..-1] survived.
+echo Prisoner (execute 2 Joe Jack William Averell Rantanplan)[-1] survived.
